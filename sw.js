fd9bd0f712f8e0c5b57b2a0338ed5192081372f5
spalumbo10 <35610743+spalumbo10@users.noreply.github.com>
2026-07-23 15:01:04 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'poker-v8';
+const CACHE = 'poker-v9';
 const FILES = ['./index.html', './admin.html', './manifest.json', './admin-manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e) {
